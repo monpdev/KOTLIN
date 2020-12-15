206a49945b82b0fcb61fd0f6e1284ada8a8c3ebc
monpdev <35469743+monpdev@users.noreply.github.com>
2020-12-15 11:27:52 +0000

    Add files via upload

---
+++ Art.kt
@@ -0,0 +1,26 @@
+// Write your code below
+
+/*
+     .....      
+  .         .   
+ .   0  0    .  
+  ...........   
+   .. __ ..     
+  . .|C_|. .    
+ .  ......  .   
+     .  .       
+     .  .       
+*/
+
+fun main() {
+  // Code is added here.
+  println("     .....")
+  println("  .         .")
+  println(" .   0  0    .")
+  println("  ...........")
+  println("   .. __ ..")
+  println("  . .|C_|. .")
+  println(" .  ......  .")
+  println("     .  .")
+  println("     .  .") 
+}
