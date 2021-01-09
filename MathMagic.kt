eb0888dcb55649767f2ee9f4613f492bf6cdf05a
monpdev <35469743+monpdev@users.noreply.github.com>
2021-01-09 22:17:28 +0000

    Add files via upload

---
+++ MathMagic.kt
@@ -0,0 +1,16 @@
+fun main() {
+  //MathMagic.kt determines the user's age using
+  //an arithmetic magic trick
+val name = "MP"
+var magicNum: Int
+
+magicNum = 2*5
+magicNum += 3
+magicNum *= 2
+magicNum += 4
+magicNum -= 6
+
+println("$name 's age is $magicNum")
+
+
+}
