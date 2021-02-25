a25961516bddc397086aca73fe6f6ded3c024f3c
monpdev <35469743+monpdev@users.noreply.github.com>
2021-02-25 01:00:47 +0000

    Add files via upload

---
+++ ColorMap.kt
@@ -0,0 +1,17 @@
+fun main() {
+  val favoriteColors = mapOf("Jesse" to "Violet", "Megan" to "Red", "Tamala" to "Blue", "Jordan" to "Pink")
+  
+  println("\n-- Key: Value Output --")
+  // Write your code below
+  for(favoriteEntry in favoriteColors){
+    println("${favoriteEntry.key}:${favoriteEntry.value}")
+  
+  println("\n-- Only Values Output --")
+  println("${favoriteEntry.value}")
+  // Write your code below
+  }
+
+  for(color in favoriteColors.values){
+    println(color)
+  }
+}
