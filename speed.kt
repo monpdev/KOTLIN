36379d610c4b1e2124bf303bddefc417378b9f6d
monpdev <35469743+monpdev@users.noreply.github.com>
2021-03-15 00:38:22 +0000

    Add files via upload

---
+++ speed.kt
@@ -0,0 +1,17 @@
+// Write your code below
+fun calculateForce(mass: Int, acceleration: Int) {
+  var force = mass * acceleration
+  println("The force is $force Newtons.")
+}
+
+fun getSpeed(distance: Int, time: Int) {
+  var speed = distance/ time
+  println("$speed meters per second")
+}
+
+
+fun main() {
+  // Write more code below
+getSpeed(10,3)
+  
+}
